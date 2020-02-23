68f63602e77d2ce32ff3fe639b3b54feaeaf57dc
Ram <ramkb466@gmail.com>
2020-02-23 10:58:04 -0500

    added jsp page for mvc controller

--- src/main/java/com/rkb/springmvc/LoginController.java
+++ src/main/java/com/rkb/springmvc/LoginController.java
@@ -8,9 +8,9 @@ import org.springframework.web.bind.annotation.ResponseBody;
 public class LoginController {
 
 	@RequestMapping(value = "/login")
-	@ResponseBody
+	//@ResponseBody
 	public String sayHello() {
-		return "hello World";
+		return "login";
 				
 	}
 }
